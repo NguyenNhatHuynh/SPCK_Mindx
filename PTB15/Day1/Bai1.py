d788feb6c6c4cc9c0b21f710384bd88aaaca8446
NguyenNhatHuynh <nguyenhuynh162003@gmail.com>
2025-07-12 11:02:16 +0700

    PTB15

---
+++ PTB15/Day1/Bai1.py
@@ -0,0 +1,15 @@
+num = int(input("Enter a number: "))
+
+if num == 2:
+    print("2 x 1 = ", 2 * 1)
+    print("2 x 1 = ", 2 * 1)
+    print("2 x 1 = ", 2 * 1)
+    print("2 x 1 = ", 2 * 1)
+    print("2 x 1 = ", 2 * 1)
+elif num == 3:
+    print("3 x 1 = ", 3 * 1)
+    print("3 x 2 = ", 3 * 2)
+elif num == 4:
+    print("4 x 1 = ", 4 * 1)
+    print("4 x 2 = ", 4 * 2)
+    print("4 x 3 = ", 4 * 3)
